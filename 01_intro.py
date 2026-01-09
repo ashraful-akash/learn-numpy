16209d429156afc1351d615bb3a39131ed1b0041
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-09 18:54:50 +0600

    Add files via upload

---
+++ 01_intro.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#python
+print(np.__version__)
+
+my_list = [1,2,3,4]
+
+my_list = my_list * 2
+
+print(my_list)
+
+#numpy
+array = np.array([1,2,3,4])
+array = array * 2
+
+print(array)
+print(type(array))
