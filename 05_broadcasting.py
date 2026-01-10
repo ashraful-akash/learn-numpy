c86c5ec3951ef60fcf092620c967f333cf653572
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-10 16:30:56 +0600

    Add files via upload

---
+++ 05_broadcasting.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# number of row / columns must match or either of them is 1
+array1 =np.array([[1,2,3,4]])
+array2 =np.array([[1],[2],[3],[4]])
+
+print(array1.shape)
+print(array2.shape)
+
+print(array1 * array2)
+
+# 
+
+array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
+array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+
+print(array1 * array2)
